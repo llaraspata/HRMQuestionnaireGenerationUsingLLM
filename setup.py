b37b2e2d2114437c3cd6b3fce6402b86786a2755
Lucrezia Laraspata <l.laraspata3@studenti.uniba.it>
2024-03-12 17:54:01 +0100

    init project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Leveraging LLMs capabilities in Questionnaire Generation for Human Resources Management',
+    author='Lucrezia Laraspata',
+    license='',
+)
