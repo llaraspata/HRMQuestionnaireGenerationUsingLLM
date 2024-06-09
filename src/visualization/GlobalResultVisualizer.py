0ca4dea3a9e895c2ecacdbb15c557af627cd7738
Lucrezia LARASPATA <llaraspata@talentia-software.com>
2024-06-10 00:37:36 +0200

    visualized global results

--- src/visualization/GlobalResultVisualizer.py
+++ src/visualization/GlobalResultVisualizer.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import plotly.graph_objects as go
 import pandas as pd
 import os
@@ -19,6 +21,9 @@ class GlobalResultVisualizer:
     TF_PRIMARY_COLOR_1 = "#D0006F"
     TF_PRIMARY_COLOR_2 = "#24135F"
 
+    TEMPERATURE_VALUES = [0, 0.25, 0.5]
+    FREQUENCY_PENALTY_VALUES = [0, 0.5, 1]
+
     # ------------
     # Constructor
     # ------------
@@ -29,6 +34,8 @@ class GlobalResultVisualizer:
         self.semantic_metrics = pd.read_csv(os.path.join(result_dir, self.SEMANTIC_METRICS_FILENAME))
         self.serendipity_scores = pd.read_csv(os.path.join(result_dir, self.SERENDIPITY_FILENAME))
         self.errors = pd.read_csv(os.path.join(result_dir, self.ERRORS_FILENAME))
+
+        self.experiment_names = self.errors["EXPERIMENT_ID"].unique()
         
 
     # ------------
@@ -51,5 +58,75 @@ class GlobalResultVisualizer:
         bw_errors_df = pd.concat([best, worst], ignore_index=True)
 
         return bw_errors_df
+    
+
+    def plot_result_heatmap(self):
+        self._plot_result_heatmap(self.errors, "CONVERSION_ERROR_RATE", "Conversion error-rate", cmap="RdYlGn_r")
+        self._plot_result_heatmap(self.errors, "JSON_ERROR_RATE", "JSON error-rate", cmap="RdYlGn_r")
+        self._plot_result_heatmap(self.intraquestionnaire_similarity, "INTRAQSTN_ROUGE_L_F1_SCORE", "Intraquestionnaire Syntactic Similarity", cmap="RdYlGn_r")
+        self._plot_result_heatmap(self.semantic_metrics, "FINAL_SCORE", "Semantic Similarity")
+        self._plot_result_heatmap(self.serendipity_scores, "AVG_SERENDIPITY_SCORE", "Serendipity Score")
+
+        self._plot_result_heatmap(self.errors, "CONVERSION_ERROR_RATE", "Conversion error-rate", cmap="viridis_r")
+        self._plot_result_heatmap(self.errors, "JSON_ERROR_RATE", "JSON error-rate", cmap="viridis_r")
+        self._plot_result_heatmap(self.intraquestionnaire_similarity, "INTRAQSTN_ROUGE_L_F1_SCORE", "Intraquestionnaire Syntactic Similarity", cmap="viridis_r")
+        self._plot_result_heatmap(self.semantic_metrics, "FINAL_SCORE", "Semantic Similarity", cmap="viridis")
+        self._plot_result_heatmap(self.serendipity_scores, "AVG_SERENDIPITY_SCORE", "Serendipity Score", cmap="viridis")
 
     
+    def _plot_result_heatmap(self, df, column, title, cmap="RdYlGn", model="", has_full_params=-1, technique=""):
+        metrics_dict = {(fp, t): [] for fp in self.FREQUENCY_PENALTY_VALUES for t in self.TEMPERATURE_VALUES}
+
+        for _, row in df[df[column].notna()].iterrows():
+            exp_id = row["EXPERIMENT_ID"]
+
+            metric = row[column]
+
+            if model and not exp_id.__contains__(model):
+                continue
+
+            if (has_full_params == 1 and not exp_id.__contains__("FULL")) or (has_full_params == 0 and exp_id.__contains__("FULL")):
+                continue
+
+            if technique and not exp_id.__contains__(technique):
+                continue
+
+            parts = exp_id.split('_')
+            
+            if parts.__contains__("JSON"):
+                parts = parts[:-1]
+            
+            if exp_id.__contains__("FULL"):
+                T_value = float(parts[4][:-1])
+                FP_value = float(parts[5][:-2])
+            else:
+                T_value = float(parts[3][:-1])
+                FP_value = float(parts[4][:-2])
+
+            metrics_dict[(FP_value, T_value)].append(metric)
+
+        metrics_matrix = np.zeros((len(self.FREQUENCY_PENALTY_VALUES), len(self.TEMPERATURE_VALUES)))
+        
+        not_performed = [[1, 0.25], [0.5, 0.5]]
+        for fp, t in not_performed:
+            FP_idx = self.FREQUENCY_PENALTY_VALUES.index(fp)
+            T_idx = self.TEMPERATURE_VALUES.index(t)
+            metrics_matrix[FP_idx, T_idx] = np.nan
+
+        for (fp, t), metrics in metrics_dict.items():
+            if metrics:
+                FP_idx = self.FREQUENCY_PENALTY_VALUES.index(fp)
+                T_idx = self.TEMPERATURE_VALUES.index(t)
+                metrics_matrix[FP_idx, T_idx] = np.mean(metrics)
+
+        plt.figure(figsize=(8, 6))
+        plt.imshow(metrics_matrix, extent=[min(self.TEMPERATURE_VALUES), max(self.TEMPERATURE_VALUES), min(self.FREQUENCY_PENALTY_VALUES), max(self.FREQUENCY_PENALTY_VALUES)],
+                   origin='lower', aspect='auto', cmap=cmap)
+        plt.colorbar(label='Values')
+        plt.xlabel("Temperature")
+        plt.ylabel("Frequency Penalty")
+        plt.title(title)
+        plt.xticks(self.TEMPERATURE_VALUES)
+        plt.yticks(self.FREQUENCY_PENALTY_VALUES)
+        plt.show()
+
