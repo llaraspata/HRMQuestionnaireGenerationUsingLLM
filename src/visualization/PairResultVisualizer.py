1789eb4be462bcb98b5c80f1777e5b461b561151
Lucrezia Laraspata <l.laraspata3@studenti.uniba.it>
2024-07-08 16:57:19 +0200

    result analysis

--- src/visualization/PairResultVisualizer.py
+++ src/visualization/PairResultVisualizer.py
@@ -22,18 +22,38 @@ class PairResultVisualizer:
     # ------------
     # Constructor
     # ------------
-    def __init__(self, pair_dict):
+    def __init__(self, pair_dict, project_root):
         self.exp_pair_id = pair_dict["id"]
         self.exp_zero_shot = pair_dict["zero-shot"]
         self.exp_one_shot = pair_dict["one-shot"]
         self.exp_zero_shot_full = pair_dict["zero-shot_full"]
         self.exp_one_shot_full = pair_dict["one-shot_full"]
 
-        self.ground_truth_questions = pd.DataFrame()
+        
         self.zero_shot_questions = pd.DataFrame()
+        self.zero_shot_answers = pd.DataFrame()
+
         self.one_shot_questions = pd.DataFrame()
+        self.one_shot_answers = pd.DataFrame()
+
         self.zero_shot_full_questions = pd.DataFrame()
+        self.zero_shot_full_answers = pd.DataFrame()
+
         self.one_shot_full_questions = pd.DataFrame()
+        self.one_shot_full_answers = pd.DataFrame()
+
+        dataset = TFQuestionnairesDataset()
+        dataset.load_data(project_root=project_root)
+        self.ground_truth_questions = dataset.questions
+        self.ground_truth_answers = dataset.answers
+
+
+    def add_pair(self, pair_dict):
+        self.exp_pair_id = pair_dict["id"]
+        self.exp_zero_shot = pair_dict["zero-shot"]
+        self.exp_one_shot = pair_dict["one-shot"]
+        self.exp_zero_shot_full = pair_dict["zero-shot_full"]
+        self.exp_one_shot_full = pair_dict["one-shot_full"]
 
 
     def load_data(self, project_root):
@@ -49,23 +69,38 @@ class PairResultVisualizer:
         one_shot_full_preds = pd.read_pickle(os.path.join(one_shot_full_dir, self.PREDICTION_FILENAME))
 
         for qst_id in zero_shot_preds["QUESTIONNAIRE_ID"].unique():
-            qst_ref = zero_shot_preds[zero_shot_preds['QUESTIONNAIRE_ID'] == qst_id]["GROUND_TRUTH_JSON"].values[0]
             qst_0s_json = zero_shot_preds[zero_shot_preds['QUESTIONNAIRE_ID'] == qst_id]["PREDICTED_JSON"].values[0]
             qst_1s_json = one_shot_preds[one_shot_preds['QUESTIONNAIRE_ID'] == qst_id]["PREDICTED_JSON"].values[0]
             qst_0s_full_json = zero_shot_full_preds[zero_shot_full_preds['QUESTIONNAIRE_ID'] == qst_id]["PREDICTED_JSON"].values[0]
             qst_1s_full_json = one_shot_full_preds[one_shot_full_preds['QUESTIONNAIRE_ID'] == qst_id]["PREDICTED_JSON"].values[0]
-
-            qst_ref = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_ref, questionnaire_id=qst_id)
-            qst_0s = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_0s_json, questionnaire_id=qst_id)
-            qst_1s = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_1s_json, questionnaire_id=qst_id)
-            qst_0s_full = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_0s_full_json, questionnaire_id=qst_id)
-            qst_1s_full = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_1s_full_json, questionnaire_id=qst_id)
-
-            self.ground_truth_questions = pd.concat([self.ground_truth_questions, qst_ref.questions], ignore_index=True)
-            self.zero_shot_questions = pd.concat([self.zero_shot_questions, qst_0s.questions], ignore_index=True)
-            self.one_shot_questions = pd.concat([self.one_shot_questions, qst_1s.questions], ignore_index=True)
-            self.zero_shot_full_questions = pd.concat([self.zero_shot_full_questions, qst_0s_full.questions], ignore_index=True)
-            self.one_shot_full_questions = pd.concat([self.one_shot_full_questions, qst_1s_full.questions], ignore_index=True)
+            
+            try:
+                qst_0s = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_0s_json, questionnaire_id=qst_id)
+                self.zero_shot_questions = pd.concat([self.zero_shot_questions, qst_0s.questions], ignore_index=True)
+                self.zero_shot_answers = pd.concat([self.zero_shot_answers, qst_0s.answers], ignore_index=True)
+            except:
+                a = 0
+            
+            try:
+                qst_1s = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_1s_json, questionnaire_id=qst_id)
+                self.one_shot_questions = pd.concat([self.one_shot_questions, qst_1s.questions], ignore_index=True)
+                self.one_shot_answers = pd.concat([self.one_shot_answers, qst_1s.answers], ignore_index=True)
+            except:
+                a = 0
+            
+            try:
+                qst_0s_full = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_0s_full_json, questionnaire_id=qst_id)
+                self.zero_shot_full_questions = pd.concat([self.zero_shot_full_questions, qst_0s_full.questions], ignore_index=True)
+                self.zero_shot_full_answers = pd.concat([self.zero_shot_full_answers, qst_0s_full.answers], ignore_index=True)
+            except:
+                a = 0
+            
+            try:
+                qst_1s_full = TFQuestionnairesDataset.from_json(project_root=os.path.abspath(os.path.join(os.getcwd(), os.pardir)), json_data=qst_1s_full_json, questionnaire_id=qst_id)
+                self.one_shot_full_questions = pd.concat([self.one_shot_full_questions, qst_1s_full.questions], ignore_index=True)
+                self.one_shot_full_answers = pd.concat([self.one_shot_full_answers, qst_1s_full.answers], ignore_index=True)
+            except:
+                a = 0
             
 
 
