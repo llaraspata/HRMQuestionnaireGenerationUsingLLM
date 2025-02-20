f3fff8bb4fefab7ac87d2901d229b671484c392d
llaraspata <l.laraspata3@studenti.uniba.com>
2025-02-20 15:18:00 +0100

    use SentenceTransformer library to compute sentence embeddings

--- src/evaluation/QuestionnairesEvaluator.py
+++ src/evaluation/QuestionnairesEvaluator.py
@@ -6,6 +6,7 @@ from rouge_score import rouge_scorer
 import numpy as np
 from openai import AzureOpenAI
 import json
+from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from src.prompts.TopicModelingScenarioGenerator import TopicModelingScenarioGenerator
 
@@ -126,12 +127,17 @@ class QuestionnairesEvaluator:
         self.client_emb = None
         self.client_gpt = None
 
+        self.sentence_emb = SentenceTransformer("all-mpnet-base-v2")
+
+        # ------------
+        # Uncomment to use Azure OpenAI for GPT-3.5-tubro and text-embedding-3-large
+        # ------------
         # self.client_emb = AzureOpenAI(
         #     api_key = os.getenv("AZURE_OPENAI_KEY"),  
         #     api_version = "2024-02-01",
         #     azure_endpoint = "https://openai-hcm-dev-d06.openai.azure.com/"
         # )
-# 
+        #
         # self.client_gpt = AzureOpenAI(
         #     azure_endpoint = "https://openai-hcm-dev-d06.openai.azure.com/", 
         #     api_key=os.getenv("AZURE_OPENAI_KEY"),  
@@ -789,7 +795,9 @@ class QuestionnairesEvaluator:
                 qst_id = generated.questions["ID"][i]
                 qst_text = generated.questions["NAME"][i]
 
-                question_embedding = QuestionnairesEvaluator.get_text_embedding(self.client_emb, qst_text)
+                # Uncomment to use OpenAI embdedding model:
+                # question_embedding = QuestionnairesEvaluator.get_text_embedding(self.client_emb, qst_text)
+                question_embedding = QuestionnairesEvaluator.get_text_embedding(self.sentence_emb, qst_text)
 
                 new_rows = self._compute_question_semantic_similarity(project_root, len(generated.questions), qst_id, i, qst_text, question_embedding, ground_truth.questions["NAME"])
 
@@ -799,27 +807,41 @@ class QuestionnairesEvaluator:
             return
         
 
-    def get_text_embedding(client, text):
-        response = client.embeddings.create(
-            input = text,
-            model= QuestionnairesEvaluator.EMBEDDING_MODEL
-        )
+    # ------------
+    # Uncomment to use text-embedding-3-large
+    # ------------
+    # def get_text_embedding(client, text):
+    #     response = client.embeddings.create(
+    #         input = text,
+    #         model= QuestionnairesEvaluator.EMBEDDING_MODEL
+    #     )
+    # 
+    #     response_json = json.loads(response.model_dump_json(indent=2))["data"]
+    #     
+    #     return response_json[0]["embedding"]
+    
 
-        response_json = json.loads(response.model_dump_json(indent=2))["data"]
-        
-        return response_json[0]["embedding"]
+    # ------------
+    # Comment to use text-embedding-3-large
+    # ------------  
+    def get_text_embedding(model, text):
+        return model.encode(text)
 
 
     def _compute_question_semantic_similarity(self, project_root, len_generated, generated_question_id, generated_question_pos, generated_question, generated_question_embedding, ground_truth_questions):
         df = pd.DataFrame(columns=self.SEMANTIC_SIMILARITY_QUESTION_COLUMNS)
 
         for j in range(len(ground_truth_questions)):
-            ground_truth_embedding = QuestionnairesEvaluator.get_text_embedding(self.client_emb, ground_truth_questions[j])
+            # Uncomment to use OpenAI embdedding model:
+            # ground_truth_embedding = QuestionnairesEvaluator.get_text_embedding(self.client_emb, ground_truth_questions[j])
+            ground_truth_embedding = QuestionnairesEvaluator.get_text_embedding(self.sentence_emb, ground_truth_questions[j])
             questions_cosine = QuestionnairesEvaluator.compute_cosine_similarity(generated_question_embedding, ground_truth_embedding)
             deviation = QuestionnairesEvaluator.compute_position_deviation_normalized(generated_question_pos, j, len_generated, len(ground_truth_questions))
             
             topic = TFQuestionnairesDataset.get_questionnaire_topic_by_id(project_root, self.questionnaire_id)
-            topic_embedding = QuestionnairesEvaluator.get_text_embedding(self.client_emb, topic)
+            # Uncomment to use OpenAI embdedding model:
+            # topic_embedding = QuestionnairesEvaluator.get_text_embedding(self.client_emb, topic)
+            topic_embedding = QuestionnairesEvaluator.get_text_embedding(self.sentence_emb, topic)
             topic_cosine = QuestionnairesEvaluator.compute_cosine_similarity(generated_question_embedding, topic_embedding)
 
             weight_sum = self.QUESTIONS_SIMILARITY_WEIGHT + self.TOPIC_SIMILARITY_WEIGHT
@@ -975,7 +997,9 @@ class QuestionnairesEvaluator:
         for question in generated_questions:
             question_topic = self.predict_question_topic(question)
             
-            question_topic_emb = QuestionnairesEvaluator.get_text_embedding(self.client_emb, question_topic)
+            # Uncomment to use OpenAI embdedding model:
+            # question_topic_emb = QuestionnairesEvaluator.get_text_embedding(self.client_emb, question_topic)
+            question_topic_emb = QuestionnairesEvaluator.get_text_embedding(self.sentence_emb, question_topic)
 
             topic_similarity = QuestionnairesEvaluator.compute_most_similar_subtopic(question_topic_emb, subtopics_embs)
 
